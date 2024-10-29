63d62005ce9b191770ff78824f4adfe8d7d6f555
unknown <leminhhai136a@gmail.com>
2024-10-29 12:52:22 +0700

    final

---
+++ admin/config.php
@@ -0,0 +1,6 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("Db_NAME", "your_db_name");
+?>
---
+++ admin/slider.php
@@ -0,0 +1,26 @@
+<section class="admin-content">
+        <div class="admin-content-left">
+            <ul>
+                <li>
+                    <a href="#">Danh mục</a>
+                    <ul>
+                        <li>Thêm danh mục</li>
+                        <li>Danh sách danh mục</li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Loại sản phẩm</a>
+                    <ul>
+                        <li>Thêm loại sản phẩm</li>
+                        <li>Danh sách loại sản phẩm</li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Sản phẩm</a>
+                    <ul>
+                        <li>Thêm sản phẩm</li>
+                        <li>Danh sách sản phẩm</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
